4b5fde7a1140889d13c2ae1c9ca163f940d3bf44
Vargattj <32938456+Vargattj@users.noreply.github.com>
2026-04-16 21:35:18 +0000

    update runs on

--- vue3-vite-ts/cypress.config.ts
+++ vue3-vite-ts/cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "buildkite-test-collector/cypress/reporter",
+  reporterOptions: {
+    token_name: "AZVrY5x32xy3UWu2N9V6QuRd",
+  },
   component: {
     devServer: {
       framework: "vue",
