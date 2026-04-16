823d6c5aa2343a7b357988e24da0c9f541ceaa98
Vargattj <32938456+Vargattj@users.noreply.github.com>
2026-04-16 22:13:39 +0000

    update cypress config

--- vue3-vite-ts/cypress.config.ts
+++ vue3-vite-ts/cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: "buildkite-test-collector/cypress/reporter",
+  reporterOptions: {
+    token_name: "BUILDKITE_ANALYTICS_TOKEN",
+  },
   component: {
     devServer: {
       framework: "vue",
@@ -8,7 +12,6 @@ export default defineConfig({
     },
   },
   e2e: {
-    reporter: "buildkite-test-collector/cypress/reporter",
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
